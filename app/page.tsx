0d938bc46df6aebd61c3aceb926244dac9efce23
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2024-05-16 19:30:23 +0100

    clerk added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import { Syne } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
 const syne = Syne({ subsets: ["latin"], weight: "800" });
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center min-h-[50dvh]">
-      <h1 className="xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl transition-all">
+    <main className="flex flex-col justify-center h-screen">
+      <h1 className="flex justify-center xs:text-3xl sm:text-6xl md:text-7xl transition-all text-primary-foreground h-1/5">
         <div className={syne.className}>STOCKA</div>
       </h1>
+      <div className="flex justify-center space-x-4">
+        <Button>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+        <Separator orientation="vertical" />
+        <Button variant="outline">
+          <Link href="/sign-in">Login</Link>
+        </Button>
+      </div>
     </main>
   );
 }
