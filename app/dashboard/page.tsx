915d078858cf04c2117d9328c70b1a64626e39a8
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2024-09-04 22:27:53 +0100

    added existing product cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,27 @@
 import { NavigationBar } from "@/components/navigation-bar";
 import { NewProductCard } from "@/components/new-product-card";
 import { Title } from "@/components/title";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
+import { createClerkSupabaseClientSsr } from "@/utils/supabase/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
 export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+  const supabase = createClerkSupabaseClientSsr();
 
-  const { data: item_details } = await supabase.from("item_details").select();
+  const { data: item_details, error } = await supabase
+    .from("item_details")
+    .select();
+  if (error) {
+    console.error("Error fetching item details:", error);
+  }
+  console.log("Item details:", item_details);
 
   return (
     <div>
@@ -21,6 +34,53 @@ export default async function Page() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 pt-10">
           <NewProductCard />
+          {item_details ? (
+            item_details.map((item) => (
+              <Card key={item.item_id}>
+                <CardHeader>
+                  <CardTitle className="text-4xl font-bold">
+                    {item.item_name}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid w-full items-center gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="cost">Item cost</Label>
+                      <p className="text-2xl font-bold text-muted-foreground">
+                        £{item.item_cost}
+                      </p>
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="price">Buy price</Label>
+                      <p className="text-2xl font-bold text-muted-foreground">
+                        £{item.buy_price}
+                      </p>
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="quantity">Quantity</Label>
+                      <p className="text-2xl font-bold text-muted-foreground">
+                        {item.quantity}
+                      </p>
+                    </div>
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <div className="text-2xl font-semibold">
+                    {item.buy_price > item.item_cost ? (
+                      <p>
+                        Currently making a{" "}
+                        <span className="text-primary">profit</span>! 🎉
+                      </p>
+                    ) : (
+                      <p>Not yet making a profit.</p>
+                    )}
+                  </div>
+                </CardFooter>
+              </Card>
+            ))
+          ) : (
+            <p>No items available</p>
+          )}
         </div>
       </div>
     </div>
